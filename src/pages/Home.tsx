bd2470abc1984296b36c11878f38cb77a3cf9c75
Ediliziaio <info@edilizia.io>
2026-04-05 13:59:06 +0200

    feat: add GaranzieSection to homepage after FAQSection

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -21,6 +21,7 @@ import PricingSection from "@/components/landing/PricingSection";
 import TestimonialsSection from "@/components/landing/TestimonialsSection";
 import FAQSection from "@/components/landing/FAQSection";
 import FinalCtaSection from "@/components/landing/FinalCtaSection";
+import GaranzieSection from "@/components/landing/GaranzieSection";
 import FounderLetterBottom from "@/components/landing/FounderLetterBottom";
 import LandingFooter from "@/components/landing/LandingFooter";
 import StickyBottomBar from "@/components/landing/StickyBottomBar";
@@ -184,6 +185,7 @@ export default function Home() {
       <PricingSection />
       <TestimonialsSection />
       <FAQSection />
+      <GaranzieSection />
       <FinalCtaSection />
       <FounderLetterBottom />
       <LandingFooter />
